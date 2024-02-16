ac9764b340c47b4fb79b523d00ec2777d4e9cda1
oscarfiscal <oscarfiscalcp@gmail.com>
2024-02-16 00:12:43 -0500

    feat: Refactor UserProfile controller and add view for displaying saved data

--- src/main/java/com/example/technicalTest/Controller/UserProfileController.java
+++ src/main/java/com/example/technicalTest/Controller/UserProfileController.java
@@ -3,18 +3,34 @@ package com.example.technicalTest.Controller;
 import com.example.technicalTest.Model.UserProfile;
 import com.example.technicalTest.Service.UserProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
+
 public class UserProfileController {
+
+    public static final String FORM_VIEW = "index";
+    public static final String RESULT_VIEW = "profileInfo";
 
     @Autowired
     private UserProfileService userProfileService;
 
-    @PostMapping("/userprofile")
-    public void createUserProfile( UserProfile userProfile) {
-        userProfileService.createUserProfile(userProfile);
+    @RequestMapping("/")
+    public String showForm(Model model) {
+        model.addAttribute("userProfile", new UserProfile());
+        return FORM_VIEW;
     }
 
+    @PostMapping("/userprofile")
+    public ModelAndView createUserProfile(@ModelAttribute("userProfile") UserProfile userProfile) {
+        userProfileService.createUserProfile(userProfile);
+        ModelAndView mav = new ModelAndView(RESULT_VIEW);
+        mav.addObject("userProfile", userProfile);
+        return mav;
+    }
 }
